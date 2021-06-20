dd9e3fd273748ac975d568aef192fe4122c3ce2c
Alex Gogl <alex@gogl.io>
2021-06-20 18:20:55 +0200

    add attributes to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,6 @@
-import { resolve } from "path";
-
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
-import dts from "vite-dts";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.tsx"),
-      name: "Hiker",
-    },
-    sourcemap: true,
-    // Reduce bloat from legacy polyfills.
-    target: "esnext",
-    // Leave minification up to applications.
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["react", "react-dom"],
-      output: {
-        // Since we publish our ./src folder, there's no point
-        // in bloating sourcemaps with another copy of it.
-        sourcemapExcludeSources: true,
-
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  },
-  plugins: [reactRefresh(), dts()],
+  plugins: [reactRefresh()],
 });
